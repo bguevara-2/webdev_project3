447e57241ede86088b395a447612a1183bf8e66c
bguevara-2 <bguevara2@hawk.iit.edu>
2025-04-15 18:28:05 -0500

    adding the actual function of scrolling up

--- js/script.js
+++ js/script.js
@@ -1,4 +1,13 @@
 function goUp() {
   const buttn = document.getElementById("toTop");
-  if(document
+  if(document.body.scrollTop > 200 || document.documentElement.scrollTop > 200){
+    buttn.style.display = "block";
+  } else {
+    buttn.style.display = "none";
+  }
 }
+;
+
+document.getElementById("toTop").onclick = function goUp() {
+  window.scrollTo({top: 0, behavior: 'smooth'});
+};
